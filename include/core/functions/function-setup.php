8359fa85fc60e458513b23b9611f7d93da4cd259
taskpul <romtietz@gmail.com>
2026-04-25 02:22:20 +0200

    Revert "Show selected variation price on single product pages"

--- include/core/functions/function-setup.php
+++ include/core/functions/function-setup.php
@@ -279,10 +279,6 @@ function adswth_scripts() {
 									}
 								}
 							});
-
-							$form.on('hide_variation', function(){
-								$price.html(defaultPriceHtml);
-							});
 						});
 					});
 				})(jQuery);"
