71a6d60562ee2fc7d33d74c4e2a030ff3e141003
taskpul <romtietz@gmail.com>
2026-04-25 07:34:30 +0200

    Make homepage slider span full viewport width

--- page-templates/page-front.php
+++ page-templates/page-front.php
@@ -4,17 +4,17 @@
  */
 ?>
 <?php get_header(); ?>
-	<div class="container mt-xl-px-20 mb-px-40">
-        <div id="content" class="content-area page-wrapper" role="main">
-            <div class="fw-block" data-sizes='["xs", "sm", "md", "lg"]'>
-                <div class="fw-back">
-                    <div class="row no-gutters">
-                        <?php get_template_part('template-parts/page-front/page-front', 'slider-menu'); ?>
-                        <?php get_template_part('template-parts/page-front/page-front', 'slider'); ?>
-                    </div>
-                </div>
-                <div class="fw-inner"></div>
+    <div class="fw-block mt-xl-px-20" data-sizes='["xs", "sm", "md", "lg"]'>
+        <div class="fw-back">
+            <div class="row no-gutters">
+                <?php get_template_part('template-parts/page-front/page-front', 'slider-menu'); ?>
+                <?php get_template_part('template-parts/page-front/page-front', 'slider'); ?>
             </div>
+        </div>
+        <div class="fw-inner"></div>
+    </div>
+	<div class="container mb-px-40">
+        <div id="content" class="content-area page-wrapper" role="main">
             <?php get_template_part('template-parts/page-front/page-front', 'countdown'); ?>
             <?php get_template_part('template-parts/page-front/page-front', 'features'); ?>
             <?php get_template_part('template-parts/page-front/page-front', 'products'); ?>
